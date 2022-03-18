5d682872b0f147c71869116eefbc45cbf68da8a4
nguyentho1 <67529723+nguyentho1@users.noreply.github.com>
2022-03-18 10:56:02 +0700

    Update EWMA_GIPr_continuousify_Tho.R

--- EWMA_GIPr_continuousify_Tho.R
+++ EWMA_GIPr_continuousify_Tho.R
@@ -1,9 +1,9 @@
-#Step 1: GIPr(phi,lambda)
+#Step 1: GIPr(phi,lambda_GIPr)
 #Step 2: normal distribution F_N(x|omega,sigma)
 #Step 3: Markov chain 
 #Step 4: ARL 
 ################################################
-# GGIPr(phi,lambda) & other equations in order to construct GIPr(phi,lambda) algorithm 
+# GGIPr(phi,lambda_GIPr) & other equations in order to construct GIPr(phi,lambda_GIPr) algorithm 
 
 #construct function g0(r, phi), g1(r, phi), g2(r, phi)
 
@@ -27,7 +27,10 @@ g1 <- function(x,phi){
   }
 }
 
-####    g2(x, phi) for E(X^2)
+## computing expected value (X) = mu_GIPr from g1
+EX_GIPr <- function(x,r,phi,lambda_GIPr){(g1(x,phi) + (r + 1 - g0(x,phi))*lambda_GIPr)/(r + 1)}
+
+####   g2(x, phi) for E(X^2)
 
 g2 <- function(x,phi){
   if(phi < 1){phi*(phi*(phi+1) - phi^(x+1)*((phi*x)^2 - phi*(2*x^2 + 2*x - 1) + (x+1)^2))/(1-phi)^3
@@ -35,17 +38,11 @@ g2 <- function(x,phi){
   else{x*(2*x^2 + 3*x + 1)/6
   }
 }
-
+## computing expected value (X^2) from g2
+EX2_GIPr <- function(x,r,phi,lambda_GIPr){(g2(x,phi) + (r + 1 - g0(x,phi))*lambda_GIPr*(lambda_GIPr + 1))/(r+1)}
 
 # compute var(X) = E(X^2) - (E(X))^2
-
-
-
-
-
-
-
-
+Var_GIPr <- EX2_GIPr - EX_GIPr^2
 #############################################
 
 #Step 2: normal distribution F_N(x|omega,sigma), f_N(x|omega,sigma)
@@ -55,13 +52,34 @@ g2 <- function(x,phi){
 #############################################
 
 #Step 3: Compute Var*(X) = Var(X) + sigma^2 
+#denote Var*(X) ~ Var(X) "continuousify"
+VarX_continuousify = Var_GIPr + sigma^2 
+
 # Compute UCL, LCL 
+UCL_continuousify <- EX_GIPr + K*sqrt((lambda_EWMA*VarX_continuousify)/(2 - lambda_EWMA))
+### we divide length intervals from 0 to UCL_continuousify into c subintervals with 2*delta width
+## c <-    (filling a certain quantity later)
+delta <- UCL_continuousify/(2*c) 
 
-#step 4: Markov chain 
+#the midpoint of the c_th subinterval is equal to (2*c - 1)/delta 
+H_j <- (2*c - 1)/delta 
+#step 4: Compute ARL1, SDRL1 based on transition probability matrix of Markov chain 
 
-#Step 5: Compute ARL1, SDRL1,...
+###ARL_GIPr_EWMA_continuousify   
+W <- matrix(0, ncol = p, nrow = p)
+p <- c + 1
 
-
+for (i in 1:p){
+  t <- (lambda_continuousify*Hj - Hj)(lambda_continuousify)
+  l <- 0 
+  while(TRUE){ 
+    t0 <- max(0, (lambda_continuosify*x + (1 - lambda_continuousify)*H(t-1)
+  
+  
+  
+  
+  
+  }
 
 
 
